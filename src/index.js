2b715befeefddf3590ac209c244cb96987b29dc3
Barabba_FDM <74730935+Barabba97@users.noreply.github.com>
2021-06-09 14:17:57 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
+const rootNode = document.querySelector("#root");
+ReactDOM.render(
+    <App displayTextDefault="Drum Machine" />, rootNode
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
